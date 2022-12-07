909c32ae1d49979da57c12fea93f031a18da5d6f
Hojin <hojin.choi@gmail.com>
2022-12-07 23:01:56 +0900

    First

---
+++ ex1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+extern void test1();
+
+int main() {
+    puts("Hello world?");
+    test1();
+    return 0;
+}
---
+++ ex2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void test1() {
+    puts("I am test1-1");
+    puts("I am test1-2");
+}
